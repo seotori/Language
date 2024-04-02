0b56a1aa1efd3fc279cbe6fa564bf7d1af689ba9
서지윤 <158802955+seotori@users.noreply.github.com>
2024-04-02 16:08:53 +0900

    김영한 실전 자바 입문 | 조건문

---
+++ Switch2.java
@@ -0,0 +1,16 @@
+package kim_basic.cond;
+
+public class Switch2 {
+	public static void main(String[] args) {
+		//grade 1:1000, 2:2000, 3:3000, 나머지: 500
+		int grade = 2;
+		
+		int coupon = switch (grade) {
+			case 1 -> 1000;
+			case 2 -> 2000;
+			case 3 -> 3000;
+			default -> 500;
+		};
+		System.out.println("발급받은 쿠폰 " + coupon);
+	}
+}
